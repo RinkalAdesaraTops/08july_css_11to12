a6bf2fa91bb58a6df01804f17d6e3f1b342c08b9
Rinkal <rinkaladesara.tops@gmail.com>
2025-09-30 16:57:12 +0530

    crd

--- Javascript/crud.js
+++ Javascript/crud.js
@@ -1,35 +1,54 @@
 //Insert 
 let data = []
-const saveData = ()=>{
+const saveData = () => {
     let nm = document.getElementById('username').value
     let age = document.frm.age.value
     let sal = document.frm.salary.value
+    let id = document.frm.userid.value
     let alldata = JSON.parse(localStorage.getItem('userinfo'))
-    let len = (alldata==null)?1:alldata.length + 1    
-    let obj =  {
-        id:len,
-        name:nm,
-        age:age,
-        salary:sal
-        
+    if (id != "") {
+        //update 
+        data = alldata.map((i)=>{
+                    if(i.id == id){
+                        i.name = nm
+                        i.age = age
+                        i.salary = sal
+                    }
+                    return i
+        })
+        // data = res
+    } else {
+        //insert
+        let len = (alldata == null) ? 1 : alldata.length + 1
+        let obj = {
+            id: len,
+            name: nm,
+            age: age,
+            salary: sal
+
+        }
+        data.push(obj)
     }
-    data.push(obj)
-    console.log(data);
-    localStorage.setItem("userinfo",JSON.stringify(data))
+
+    localStorage.setItem("userinfo", JSON.stringify(data))
     document.frm.reset()
+    document.frm.userid.value = ''
     disp()
 }
-const disp=()=>{
+const disp = () => {
     let dt = localStorage.getItem("userinfo")
     let res = JSON.parse(dt)
-    let tr=''
-    res.map((i)=>{
+    let tr = ''
+    res.map((i) => {
         tr += `<tr>
                 <td>${i.id}</td>
                 <td>${i.name}</td>
                 <td>${i.age}</td>
                 <td>${i.salary}</td>
-                <td><button class="btn btn-danger" onclick="delData(${i.id})">Delete</button></td>
+                <td>
+                <button class="btn btn-primary" onclick="editData(${i.id})">Edit</button>
+                <button class="btn btn-danger" onclick="delData(${i.id})">Delete</button>
+                </td>
                 </tr>
         `
         // tr += "<tr><td>"
@@ -37,13 +56,22 @@ const disp=()=>{
     })
     document.getElementById('alldata').innerHTML = tr
 }
-const delData = (id)=>{
+const delData = (id) => {
     let alldata = JSON.parse(localStorage.getItem('userinfo'))
-    //1 2 3 4 5 6 id=4
-    let res = alldata.filter((i)=>{
-            return i.id != id
+    let res = alldata.filter((i) => {
+        return i.id != id
     })
-    localStorage.setItem("userinfo",JSON.stringify(res))
+    localStorage.setItem("userinfo", JSON.stringify(res))
     disp()
+}
+const editData = (id) => {
+    let alldata = JSON.parse(localStorage.getItem('userinfo'))
+    let res = alldata.find((i) => {
+        return i.id == id
+    })
+    document.frm.age.value = res.age
+    document.frm.username.value = res.name
+    document.frm.salary.value = res.salary
+    document.frm.userid.value = id
 }
 disp()
