2e9b29621ee875b21fa68e889e2c1e2c50368d01
Rinkal <rinkaladesara.tops@gmail.com>
2025-10-03 12:54:19 +0530

    crd

---
+++ Javascript/Revision/usercrud.js
@@ -0,0 +1,71 @@
+// let btn = document.getElementById('save')
+// btn.addEventListener("click",()=>{
+
+// })
+let userData = []
+const saveData = () => {
+    let nm = document.frm.name.value
+    let ag = document.frm.age.value
+    let sl = document.frm.salary.value
+    let userid = document.frm.userid.value
+    let alldata = JSON.parse(localStorage.getItem("UserData"))
+    if (userid != '') {
+        //updtae
+        userData = alldata.map((i)=>{
+                        if(i.id == userid){
+                            i.name = nm
+                            i.age = ag
+                            i.salary = sl
+                        }
+                    return i
+        })
+        // userData = res
+    } else {
+        //insert
+        let len = alldata != null ? alldata.length + 1 : 1
+        let obj = {
+            "id": len,
+            "name": nm,
+            "age": ag,
+            "salary": sl
+        }
+        userData.push(obj)
+    }
+
+    localStorage.setItem("UserData", JSON.stringify(userData))
+    dispData()
+    document.frm.reset()
+}
+const dispData = () => {
+    let alldata = JSON.parse(localStorage.getItem("UserData"))
+    let tr = ""
+    alldata.map((i) => {
+        tr += `<tr>
+                    <td>${i.id}</td>
+                    <td>${i.name}</td>
+                    <td>${i.age}</td>
+                    <td>${i.salary}</td>
+                    <td>
+                        <button onClick="editData(${i.id})">Edit</button>
+                        <button onClick="delData(${i.id})">Delete</button>
+                    </td>
+                </tr>
+        `
+    })
+    document.getElementById("allusers").innerHTML = tr
+}
+const delData = (id) => {
+    let alldata = JSON.parse(localStorage.getItem("UserData"))
+    let res = alldata.filter((i) => i.id != id)
+    localStorage.setItem("UserData", JSON.stringify(res))
+    dispData()
+}
+const editData = (id) => {
+    let alldata = JSON.parse(localStorage.getItem("UserData"))
+    let res = alldata.find((i) => i.id == id)
+    document.frm.name.value = res.name
+    document.frm.age.value = res.age
+    document.frm.salary.value = res.salary
+    document.frm.userid.value = id
+}
+dispData()
